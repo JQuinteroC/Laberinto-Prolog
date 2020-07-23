405e8d9deea4bb74b2960bc2f6642bf02a12afa3
JQuinteroC <jlqc1772@gmail.com>
2020-07-23 02:04:29 -0500

    Fin

--- gifs.py
+++ gifs.py
@@ -2,29 +2,26 @@ from PIL import Image, ImageDraw, ImageFont
 import imageio
 
 class gif():
-    def __init__(self):
-        self.a = [[0,2,3],[4,5,6],[7,8,9]]
+    def __init__(self, mapa):
+        self.a = mapa
+        self.reco = []
         self.nombres = []
         self.images = []
-        
-        self.crearImagen(self.a,2)
-        self.a = [[1,0,3],[4,5,6],[7,8,9]]
-        self.crearImagen(self.a,3)
-        self.a = [[1,2,0],[4,5,6],[7,8,9]]
-        self.crearImagen(self.a,4)
-        self.a = [[1,2,3],[0,5,6],[7,8,9]]
-        self.crearImagen(self.a,5)
-        self.a = [[1,2,3],[4,0,6],[7,8,9]]
-        self.crearImagen(self.a,6)
-        self.a = [[1,2,3],[4,5,0],[7,8,9]]
-        self.crearImagen(self.a,7)
-        self.a = [[1,2,3],[4,5,6],[0,8,9]]
-        self.crearImagen(self.a,8)
-        self.a = [[1,2,3],[4,5,8],[7,0,9]]
 
+        self.crearImagen(self.a, 2, -1, -1)
+
+
+
+    def mover(self, recorrido):
+        self.reco = recorrido
+        num = 3
+        for i in self.reco:
+            pos = buscar_en_matriz(self.a,0,i)
+            self.crearImagen(self.a,num,pos[0],pos[1])
+            num = num + 1
         self.crearGif()
     
-    def crearImagen(self,a,paso):
+    def crearImagen(self,a,paso, posi, posj):
         x = 0
         y = 0
     
@@ -35,9 +32,19 @@ class gif():
 
         for i in range(len(a)):
             for j in range(len(a[i])):
-                draw.text((50 + x,50 + y),str(a[i][j]),font=font, fill="black")
-                x += 100
-            y += 100
+                if i == posi and j == posj:
+                    draw.text((50 + x,50 + y),str(a[i][j]),font=font, fill="red")
+                else:
+                    if (j == 0 or j == len(a[i])) and a[i][j] == "+":
+                        draw.text((50 + x,50 + y),"|",font=font, fill="black")
+                    elif str(a[i][j]) == "-":
+                        draw.text((50 + x,50 + y)," ",font=font, fill="black")
+                    elif str(a[i][j]) == "+":
+                        draw.text((50 + x,50 + y),"─",font=font, fill="black")
+                    else:
+                        draw.text((50 + x,50 + y),str(a[i][j]),font=font, fill="black")
+                x += 50
+            y += 50
             x = 0
 
         image.save("resources/Imagen"+ str(paso) +".png")
@@ -47,11 +54,21 @@ class gif():
         for i in range(len(self.nombres)):
             self.images.append(imageio.imread(self.nombres[i]))
 
-        imageio.mimsave('resources/movie.gif',self.images)
+        imageio.mimsave('resources/movie.gif',self.images, duration=0.5, subrectangles=True)
 
 def main():
     gif()
     return 0
+
+def buscar_en_matriz(matriz, cont, para):
+    if matriz == []:
+        return (-1, -1)
+    paraX = str(para) + "|"
+    if str(para) in matriz[0]:
+        return [cont, matriz[0].index(str(para))]
+    elif paraX in matriz[0]:
+        return [cont, matriz[0].index(str(paraX))]
+    return buscar_en_matriz(matriz[1:], cont + 1, para)
 
 if __name__ == '__main__':
     main()
--- laberinto.py
+++ laberinto.py
@@ -1,4 +1,5 @@
 from pyswip import Prolog
+from gifs import gif
 prolog = Prolog()
 # Lectura del archivo
 mapa = open("laberinto.txt")
@@ -6,24 +7,6 @@ map = []
 for i in mapa:
     map += [i.split(" ")]
 
-# LLenar las horizontales
-#  ESTO YA FUNCIONA Y LO MANDA A PROLOG, PERO PRIMERO PRUEBAS :3
-'''for i in range(1,len(map),2):
-    for j in range(1, len(map[i])-2):
-        if "|" in map[i][j]:
-            print("tiene la cosa")
-        else:
-            prolog.assertz("conectado(" + map[i][j].replace('|', '') + "," + map[i][j+1].replace('|', '') + ")")
-            '''
-
-# LLenar las verticales
-for i in range(1,len(map)-2):
-    for j in range(0,len(map[i])-1):
-        if "-" in map[i+1][j]:
-          if("conectado(" + map[i][j].replace('|', '') + "," + map[i+2][j].replace('|', '') + ")" != "conectado(,)"):
-            prolog.assertz("conectado(" + map[i][j].replace('|', '') + "," + map[i+2][j].replace('|', '') + ")") """
-
-# ESTO ES PARA BUSCAR DONDE ESTA EL COMIENZO Y EL FIN, AUN FALTARIA BUSCAR DONDE ESTA LA POSICION ANTERIOR POR LA QUE PODRIA LLEGAR
 def buscar_en_matriz(matriz, cont, para):
     if matriz == []:
         return (-1, -1)
@@ -31,37 +14,67 @@ def buscar_en_matriz(matriz, cont, para):
         return [cont, matriz[0].index(para)]
     return buscar_en_matriz(matriz[1:], cont + 1, para)
 
+def caminoLlegada(pos, k, j):
+    alf = ["+", "|", "-"]
+    try:
+        for i in alf:
+            temStr = map[pos[0]+k][pos[1]+j].replace(i, "")
+        temInt = int(temStr)
+        prolog.assertz(
+            "conecta(" + map[pos[0]][pos[1]] + "," + temStr + ")")
+    except:
+        pass
 
-print(buscar_en_matriz(map, 0, "inicio"))  # Inicio
-print(buscar_en_matriz(map, 0, "fin"))  # Fin
+def inicializar():
+    ini = buscar_en_matriz(map, 0, "inicio")  # Inicio
+    caminoLlegada(ini, 1, 0)
+    caminoLlegada(ini, 0, 1)
+    caminoLlegada(ini, -1, 0)
+    caminoLlegada(ini, 0, -1)
 
-""" prolog.assertz("conecta(inicio, 2)")
-prolog.assertz("conecta(1, 7)")
--prolog.assertz("conecta(2, 3)")
-prolog.assertz("conecta(2, 8)")
--prolog.assertz("conecta(3, 4)")
-prolog.assertz("conecta(3, 9)")
-prolog.assertz("conecta(4, 10)")
--prolog.assertz("conecta(5, 6)")
-prolog.assertz("conecta(5, 11)")
-prolog.assertz("conecta(7, 13)")
--prolog.assertz("conecta(8, 9)")
-prolog.assertz("conecta(10, 16)")
-prolog.assertz("conecta(11, 17)")
-prolog.assertz("conecta(12, 18)")
--prolog.assertz("conecta(13, 14)")
--prolog.assertz("conecta(14, 15)")
-prolog.assertz("conecta(14, fin)")
-prolog.assertz("conecta(16, 22)")
-prolog.assertz("conecta(22, 21)")
-prolog.assertz("conecta(21, 15)")
+    # LLenar las horizontales
+    for i in range(1, len(map), 2):
+        for j in range(1, len(map[i])-2):
+            if not("|" in map[i][j]):
+                prolog.assertz(
+                    "conecta(" + map[i][j].replace('|', '') + "," + map[i][j+1].replace('|', '') + ")")
 
-prolog.assertz('conectado(Pos1,Pos2) :- conecta(Pos1,Pos2)')
-prolog.assertz('conectado(Pos1,Pos2) :- conecta(Pos2,Pos1)')
-prolog.assertz('miembro(X,[X|_])')
-prolog.assertz('miembro(X,[_|Y]) :- miembro(X,Y)')
-prolog.assertz('sol :- camino([inicio],Sol),write(Sol)')
-prolog.assertz('camino([fin|RestoDelCamino],[fin|RestoDelCamino])')
-prolog.assertz('camino([PosActual|RestoDelCamino],Sol) :- conectado(PosActual,PosSiguiente),\+ miembro(PosSiguiente,RestoDelCamino), camino([PosSiguiente,PosActual|RestoDelCamino],Sol)')
+    # LLenar las verticales
+    for i in range(1, len(map)-2):
+        for j in range(0, len(map[i])-1):
+            if "-" in map[i+1][j]:
+                if("conecta(" + map[i][j].replace('|', '') + "," + map[i+2][j].replace('|', '') + ")" != "conecta(,)"):
+                    prolog.assertz(
+                        "conecta(" + map[i][j].replace('|', '') + "," + map[i+2][j].replace('|', '') + ")")
 
-print(str(prolog.query("sol"))) """
+    # Llenar la posicion de inicio y de final
+    fin = buscar_en_matriz(map, 0, "fin")  # Fin
+    caminoLlegada(fin, 1, 0)
+    caminoLlegada(fin, 0, 1)
+    caminoLlegada(fin, -1, 0)
+    caminoLlegada(fin, 0, -1)
+
+    # Funciones de busqueda
+    prolog.assertz('conectado(Pos1,Pos2) :- conecta(Pos1,Pos2)')
+    prolog.assertz('conectado(Pos1,Pos2) :- conecta(Pos2,Pos1)')
+    prolog.assertz('miembro(X,[X|_])')
+    prolog.assertz('miembro(X,[_|Y]) :- miembro(X,Y)')
+    prolog.assertz('sol :- camino([inicio],Sol),write(Sol)')
+    prolog.assertz('camino([fin|RestoDelCamino],[fin|RestoDelCamino])')
+    prolog.assertz('camino([PosActual|RestoDelCamino],Sol) :- conectado(PosActual,PosSiguiente),\+ miembro(PosSiguiente,RestoDelCamino), camino([PosSiguiente,PosActual|RestoDelCamino],Sol)')
+    
+    # Realizar busqueda
+    camino = []
+    for inf in prolog.query("camino([inicio],Sol)"):
+        for j in inf["Sol"]:
+            camino += [j]
+        break
+    camino.reverse()
+    camino[0] = "inicio"
+    camino[-1] = "fin"
+    return camino
+
+camino = inicializar()
+
+gi = gif(map)
+gi.mover(camino)
